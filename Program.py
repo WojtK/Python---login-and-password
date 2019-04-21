4d8120f4a1c5214114c3ea0b2436317fba3ebfdc
WojtK <48882476+WojtK@users.noreply.github.com>
2019-04-22 01:08:07 +0200

    Create Program.py

---
+++ Program.py
@@ -0,0 +1,10 @@
+# Program saves login and password, and display it on screen
+
+print("Write your login ")
+login = input()
+print("Write your password")
+password = input()
+
+print("Your login: "+login)
+print("Your password: "+password)
+
